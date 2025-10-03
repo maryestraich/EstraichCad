62bc10811442fe469e69e71d22b6a4acb8de1cef
mary.estraich <maria.estraich@escola.pr.gov.br>
2025-10-03 00:09:38 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js"></scri
+pt>
+
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.28/jspdf.plugin.a
+utotable.min.js"></script>
+
+<script src="script.js"></script>
+
+</body>
+
+</html>
